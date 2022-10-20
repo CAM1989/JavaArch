8fd71e4adf0efc214665db2931f5ac8647ab5001
Andrey <cherepovam@gmail.com>
2022-10-20 16:47:36 +0300

    hw3

--- src/main/java/ru/gb/Lesson2/ResponseSerializer.java
+++ src/main/java/ru/gb/Lesson2/ResponseSerializer.java
@@ -1,20 +1,8 @@
 package ru.gb.Lesson2;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import ru.gb.Lesson2.domain.HttpResponse;
 
 public interface ResponseSerializer {
 
-    default Reader answerRequest(Path paths) throws IOException {
-
-        return Files.newBufferedReader(paths);
-
-    }
-
-    default Reader dontAnswer(Path path1) throws IOException{
-        return new StringReader("<h1>Error!!!</h1>");
-    }
+    String serialize(HttpResponse httpResponse);
 }
