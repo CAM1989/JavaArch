1eda89c73b0f7e67b6014a733ab6afd5c430439b
Andrey <cherepovam@gmail.com>
2022-10-24 13:47:51 +0300

    hw5

---
+++ src/main/java/ru/gb/Lesson2/handler/Handler.java
@@ -0,0 +1,12 @@
+package ru.gb.Lesson2.handler;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Handler {
+    int order();
+}
